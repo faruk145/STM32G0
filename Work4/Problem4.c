df50c2f3ea090e5468fbc4d3100480e0d423fcfd
faruk145 <59957778+faruk145@users.noreply.github.com>
2021-02-19 20:29:06 +0100

    Create Problem4.c

---
+++ Work4/Problem4.c
@@ -0,0 +1,37 @@
+/*
+author: Ömer Faruk Aydın Problem4
+171024002
+*/
+#include "stm32g0xx.h"
+
+#define LEDDELAY    1600000
+
+void delay(volatile uint32_t);
+
+
+int main(void) {
+
+    /* Enable GPIOC clock */
+    RCC->IOPENR |= (1U << 2);
+
+    /* Setup PC6 as output */
+    GPIOC->MODER &= ~(3U << 2*6);
+    GPIOC->MODER |= (1U << 2*6);
+    /* Turn on LED */
+    GPIOC->ODR |= (1U << 6);
+
+    while(1) {
+        delay(LEDDELAY);
+        /* Toggle LED */
+        GPIOC->ODR ^= (1U << 6);
+        for(;;){
+        IWDG -> KR |= 0x0000CCCC; //Independent watchdog to escape infinite loop.
+        }
+    }
+
+    return 0;
+}
+
+void delay(volatile uint32_t s) {
+    for(; s>0; s--);
+}
